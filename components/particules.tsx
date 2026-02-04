bfdf53fa354144f4c4de55d5dd6d543ecb98d52f
orikuOlarotimi <rotimipraise07@gmail.com>
2026-02-04 08:07:58 +0100

    started: main website structure

---
+++ components/particules.tsx
@@ -0,0 +1,6 @@
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+// import { loadAll } from "@tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
+// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the
